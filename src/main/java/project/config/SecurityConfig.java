7939fdf9657a730cd5e93db3a70973ca30fda1a4
kim11125 <bd-db@naver.com>
2022-02-14 18:12:01 +0900

    새 프로젝트 시작 220214 18:11

---
+++ src/main/java/project/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	//접근권한 관련
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/").permitAll()
+			//나머지는 인증
+			.anyRequest().authenticated()
+			;
+	}
+	
+	//패스워드 암호화
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	//무시처리
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			;
+	}
+	
+	
+	
+
+}
